2ba137478a6cc0d32911c16b7b254d9c828109d7
14126 <14126@A001-15.nordakademie.intern>
2015-10-20 10:47:04 +0200

    Lösung nach J.Mörker und E.Kapitza

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,19 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			double a = 8;
+			double b = 3; 
+			
+			double loesung1 = (a*a)+(2*a*b)+(b*b);
+			double loesung2 = (a+b)*(a+b);
+					
+		System.out.println("Lösung 1 =" + loesung1);
+		System.out.println("Lösung 2 =" + loesung2);
+
+	}
+
+}
